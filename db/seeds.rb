fddab530c54ce05246fd8637fa132af364097721
Obed Osei Frimpong <obedosei@gmail.com>
2014-12-15 18:39:45 +0800

    Yano 101

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+AdminUser.create!(:email => 'obedosei@gmail', :password => 'Yano202', :password_confirmation => 'Yano202')
